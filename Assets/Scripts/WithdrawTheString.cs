ebe1cef586463f067cc36f6b716d0f4077821001
xa20asd <xa20asd@gmail.com>
2025-02-27 00:55:08 +0800

    新增圖鑑開啟功能

--- Assets/Scripts/WithdrawTheString.cs
+++ Assets/Scripts/WithdrawTheString.cs
@@ -23,6 +23,34 @@ public class WithdrawTheString : MonoBehaviour, IPointerDownHandler, IPointerUpH
     [SerializeField, Header("釣魚成功")]
     public GameObject get;
 
+    string[,,] fish =
+        {
+            {
+                {"櫻花鉤吻鮭","台灣鯉","台灣鰻","美麗鮎","金線魚"},
+                {"溪鰻","鯔魚","鰱魚","台灣錦鯉","黑鱸"},
+                {"大頭鰻","橙帶鰻","尖吻鮎","美洲鯰","台灣黃鱸"}
+            },
+            {
+                {"草魚","紅目鯛","斑點鯰","長吻鯛","斑馬魚"},
+                {"青魚","吳郭魚","黑鯰","紅斑鯰","銀鯉"},
+                {"鮒魚","台灣鯰","黃金鯉","銀鮭","烏鰱"}
+            },
+            {
+                {"鯖魚","旗魚","鯛魚","石斑魚","赤鯛"},
+                {"金目鯛","鱸魚","魯魚","大眼鯛","竹筴魚"},
+                {"魷魚","海鱸","鰹魚","真鯉","多鰭魚"}
+            }
+        };
+    int[,,] fishPrice =
+        {
+            {
+                {40,25,18, },
+                { },
+                { }
+            },
+           
+        };
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -46,10 +74,14 @@ public class WithdrawTheString : MonoBehaviour, IPointerDownHandler, IPointerUpH
                 throwString.SetActive(false);
                 tensionButton.SetActive(false);
                 withdrawButton.SetActive(false);
+                if (PlayerPrefs.GetString("fisharea") == "溪邊 1 區")
+                { 
+                    
+                }
                 theFish.text = "櫻花鉤吻鮭";
                 theFishPrice.SetActive(true);
             }
-            if (theFishStatus.text == "-- 緊 --")
+            if (theFishStatus.text == "== 緊 ==")
             {
                 stringTensionTime += Time.deltaTime;
                 if (stringTensionTime >= 4.0f)
---
+++ Assets/Scripts/illustrateBookOpen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class illustrateBookOpen : MonoBehaviour
+{
+    [SerializeField,Header("按鈕文字")]
+    public Text theButtonText;
+    public Text[] elseButtonText;
+
+    [SerializeField, Header("各地區魚類圖鑑")]
+    public GameObject theFishData;
+    public GameObject[] elseFishData;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenFishData()
+    {
+        theButtonText.fontStyle = FontStyle.Bold;
+        for (int i = 0; i < elseFishData.Length; i++)
+        {
+            elseButtonText[i].fontStyle = FontStyle.Normal;
+        }
+
+        theFishData.SetActive(true); 
+        for (int k = 0; k < elseFishData.Length; k++)
+        { 
+            elseFishData[k].SetActive(false);
+        }
+    }
+
+}
